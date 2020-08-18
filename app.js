a41664fc08121a7437d3086889c0f6f07b3a1bff
ashley kildunne <ashleykildunne@ashleys-MBP.attlocal.net>
2020-08-18 08:33:24 -0700

    Select Value Complete

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 // `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${name}`
 // `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
 
-const getLiquor = async () => {
-  const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${name}`
-}
-
+// Mick helped me with this part 
 let liquorSelect = document.querySelector('.liquor-select')
 let liquorName = document.createElement('option')
 liquorName.value = ''
-liquorName.innerHTML = 'Choose your Liquor'
+liquorName.innerHTML = 'Pick Your Poison'
 liquorSelect.append(liquorName)
 const liquor = ['Bourbon', 'Gin', 'Mezcal', 'Rum', 'Scotch', 'Tequila', 'Vodka', 'Whiskey']
 liquor.forEach((name) => {
@@ -16,4 +13,20 @@ liquor.forEach((name) => {
   liquorName.value = name
   liquorName.innerHTML = name
   liquorSelect.append(liquorName)
+})
+//
+
+// Followed Thursday's lesson 
+const getLiquor = async (name) => {
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${name}`
+  try {
+    const response = await axios.get(url)
+    console.log(response)
+  } catch (error) {
+    console.log(`Error: ${error}`)
+  }
+}
+
+liquorSelect.addEventListener('change', () => {
+  getLiquor(liquorSelect.value)
 })
