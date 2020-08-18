42593229ccd528d432295e8101ef0d55dc618ebb
ashley kildunne <ashleykildunne@ashleys-MBP.attlocal.net>
2020-08-18 09:30:59 -0700

    Id Function in Progress

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 // Mick helped me with this part 
 let liquorSelect = document.querySelector('.liquor-select')
 let liquorName = document.createElement('option')
+let results = document.querySelector('.results')
 liquorName.value = ''
 liquorName.innerHTML = 'Pick Your Poison'
 liquorSelect.append(liquorName)
@@ -21,7 +22,20 @@ const getLiquor = async (name) => {
   const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${name}`
   try {
     const response = await axios.get(url)
-    console.log(response)
+    // console.log(response.data.drinks)
+    response.data.drinks.forEach((drink) => {
+      console.log(drink)
+      let drinkResultWrapper = document.createElement('div')
+      drinkResultWrapper.className = 'drink-result-wrapper'
+      drinkResultWrapper.append(drink.strDrink)
+      let img = document.createElement('img')
+      img.src = drink.strDrinkThumb
+      drinkResultWrapper.append(img)
+      results.append(drinkResultWrapper)
+      drinkResultWrapper.addEventListener('click', () => {
+        getLiquorDetails(drink.idDrink)
+      })
+    })
   } catch (error) {
     console.log(`Error: ${error}`)
   }
@@ -29,4 +43,20 @@ const getLiquor = async (name) => {
 
 liquorSelect.addEventListener('change', () => {
   getLiquor(liquorSelect.value)
+})
+
+
+async function getLiquorDetails(id) {
+  // console.log('test', id)
+  const idUrl = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+  try {
+    const response = await axios.get(idUrl)
+    let id = drink.idDrink
+  } catch (error) {
+    console.log(`Error: ${error}`)
+  }
+}
+
+drinkResultWrapper.addEventListener('click', () => {
+  getLiquorDetails(drinkResultWrapper.value)
 })
